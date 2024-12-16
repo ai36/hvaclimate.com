03a012a8ef844edf388c50ee981ad99a317bcee9
Andrei Fedorov <155785416+ai36@users.noreply.github.com>
2024-12-15 17:09:32 -0800

    Delete _ directory

--- _/site_v003_внедрение gulp+favicon+lighthouse/gulp/tasks/images.js
+++
@@ -1,5 +0,0 @@
-export const images = () => {
-  return app.gulp.src(app.path.src.images)
-    .pipe(app.gulp.dest(app.path.build.images))
-    .pipe(app.plugins.browsersync.stream())
-}
--- _/site_v005_выпилены нецелевые услуги+проработан дизайн+sitemap/gulp/config/plugins.js
+++
@@ -1,7 +0,0 @@
-import replace from "gulp-replace";
-import browsersync from "browser-sync";
-
-export const plugins = {
-    replace: replace,
-    browsersync: browsersync,
-}

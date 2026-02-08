8d1064babb6f80f20965506c3f695682c617d354
Andrei Fedorov <4363626@gmail.com>
2026-02-07 21:37:02 -0800

    Preparing for deployment on vercel #2

--- gulpfile.js
+++ gulpfile.js
@@ -3,10 +3,10 @@ import { path } from "./gulp/config/path.js";
 import { plugins } from "./gulp/config/plugins.js";
 
 global.app = {
-    path: path,
-    gulp: gulp,
-    plugins: plugins,
-}
+  path: path,
+  gulp: gulp,
+  plugins: plugins,
+};
 
 import { copy } from "./gulp/tasks/copy.js";
 import { reset } from "./gulp/tasks/reset.js";
@@ -17,15 +17,20 @@ import { js } from "./gulp/tasks/js.js";
 import { images } from "./gulp/tasks/images.js";
 
 function watcher() {
-    gulp.watch(path.watch.files, copy);
-    gulp.watch(path.watch.html, html);
-    gulp.watch(path.watch.css, css);
-    gulp.watch(path.watch.js, js);
-    gulp.watch(path.watch.images, images);
+  gulp.watch(path.watch.files, copy);
+  gulp.watch(path.watch.html, html);
+  gulp.watch(path.watch.css, css);
+  gulp.watch(path.watch.js, js);
+  gulp.watch(path.watch.images, images);
 }
 
 const mainTasks = gulp.parallel(copy, html, css, js, images);
 
-const dev = gulp.series(reset, mainTasks, gulp.parallel(watcher, server));
+// ✅ Production build (заканчивается)
+export const build = gulp.series(reset, mainTasks);
 
-gulp.task('default', dev);
+// ✅ Local dev (следит за файлами и поднимает сервер)
+export const dev = gulp.series(reset, mainTasks, gulp.parallel(watcher, server));
+
+// default пусть будет dev для локалки
+gulp.task("default", dev);
